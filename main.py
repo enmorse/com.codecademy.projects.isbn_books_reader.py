9e475db19f524371a71922465cfc5a16e62f34a4
Ernest Morse <enmorse48@gmail.com>
2022-08-03 18:37:39 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import csv
+
+with open('books.csv') as books_csv:
+    books_reader = csv.DictReader(books_csv, delimiter='@')
+
+    isbn_list = []
+
+    for book in books_reader:
+        isbn_list.append(book['ISBN'])
